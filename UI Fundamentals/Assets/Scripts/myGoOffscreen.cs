464aa3e2d07397d581bc00239a01d2d5bd1cd57b
Aaron Kang <ajkang2@gmail.com>
2021-03-18 18:46:20 -0700

    Push Unity UI Projects
    1) Prototype 5 is about learning the basics of Unity UI
    2) UI Fundamentals is about learning advanced Unity UI techniques and applying them to a project

---
+++ UI Fundamentals/Assets/Scripts/myGoOffscreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myGoOffscreen : MonoBehaviour
+{
+    public List<GameObject> MoveObjects;
+    
+    public void moveOffscreen()
+    {
+        foreach(GameObject obj in MoveObjects)
+        {
+            obj.GetComponent<Animator>().SetBool("onScreen", false);
+        }
+    }
+}
