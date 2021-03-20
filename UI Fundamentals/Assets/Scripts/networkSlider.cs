2621f073e3b30b21078f56dcca006f61c2952352
Aaron Kang <ajkang2@gmail.com>
2021-03-19 18:12:13 -0700

    Created sliders
    Created sliders that changed volume of music and sound effects via audio mixer groups.

---
+++ UI Fundamentals/Assets/Scripts/networkSlider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class networkSlider : MonoBehaviour
+{
+    public Text onText;
+    public Text offText;
+
+    public void setText (float sliderValue)
+    {
+        if(sliderValue < 0.5f)
+        {
+            onText.color = Color.white;
+            offText.color = Color.red;
+        } else
+        {
+            onText.color = Color.green;
+            offText.color = Color.white;
+        }
+    }
+}
